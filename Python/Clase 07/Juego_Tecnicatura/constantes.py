3ee97b3fb53f0f0b0e488a2b24a9eae414c70273
Matias Gomez <you@example.com>
2025-09-22 20:31:55 -0300

    Clase 07 Proyecto nuevo Parte 1

---
+++ Python/Clase 07/Juego_Tecnicatura/constantes.py
@@ -0,0 +1,12 @@
+import os
+
+#Dimension de la pantalla, el alto y el ancho
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+#Colores de la pantalla
+
+COLOR_LASER = (0, 0, 255) #Azul
+
+#Ruta a la carpeta assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
