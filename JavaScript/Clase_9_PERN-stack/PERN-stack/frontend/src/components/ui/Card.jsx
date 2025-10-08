aa40cd37089a30212021b6273f6533cf9d2e7adc
Matias Gomez <you@example.com>
2025-10-07 22:20:50 -0300

    Clase 09 Proyecto con stack pern por Daniel Guerrero -> Parte 5

---
+++ JavaScript/Clase_9_PERN-stack/PERN-stack/frontend/src/components/ui/Card.jsx
@@ -0,0 +1,11 @@
+// src/components/ui/Card.jsx
+
+// src/components/ui/Card.jsx
+
+export function Card({ children }) {
+  return (
+    <div className="bg-zinc-900 p-14 rounded-md">
+      {children}
+    </div>
+  );
+}
