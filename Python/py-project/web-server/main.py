147ddafefe3c376057193c1e6098c4b6ddef847e
José Luis Villalba <joseluisvillalba11@gmail.com>
2025-11-04 17:55:20 -0300

    Agregando FastApi al proyecto

--- Python/py-project/web-server/main.py
+++ Python/py-project/web-server/main.py
@@ -1,10 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI() # Crear una instancia de la aplicación FastAPI
+
+# Ruta principal
+@app.get('/') # agregamos el decorador para definir la ruta raíz
+def get_list():
+    return [1, 2, 3,]
+
+# Ruta de contacto o segunda ruta
+@app.get('/contact', response_class=HTMLResponse) # especificamos que la respuesta será HTML
+def get_list():
+    return '''
+    <h1>Hola soy un sitio web</h1>
+    <p>Soy un párrafo para ser leído</p>
+    '''
 
 def run():
-    razas = store.get_razas()
-    for raza in razas:
-        print(raza['name'])
+    store.get_razas()
 
 
 if __name__ == '__main__':
